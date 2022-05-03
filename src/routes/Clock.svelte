5517209fab1af6e6bdbe31acaf2bf91e18ee6914
threegigs <yanghuaxuan@gmail.com>
2022-05-03 00:30:59 -0700

    Switch to sveltekit

---
+++ src/routes/Clock.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import {Clock} from "../etc/Clock"
+    export const formatter = new Intl.DateTimeFormat("en", {
+        hour12: true,
+        hour: "numeric",
+        minute: "2-digit",
+        second: "2-digit",
+    })
+</script>
+
+<h3>{formatter.format($Clock)}</h3>
---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import Clock from "./Clock.svelte"
+</script>
+
+<div style="text-align: center">
+    <h1>Welcome to Mastodor</h1>
+    <Clock />
+</div>
